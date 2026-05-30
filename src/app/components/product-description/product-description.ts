9b9448a2e755e1aa1ad06d777cc565bd679c0348
migel <migelangelo10@gmail.com>
2026-05-30 05:14:02 +0300

    product description added

--- src/app/components/product-description/product-description.ts
+++ src/app/components/product-description/product-description.ts
@@ -1,21 +1,28 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal, WritableSignal } from '@angular/core';
 import { ProductType } from '../../products/types/product-type';
 import { ModalFeatures } from '../../modals/modal-features/modal-features';
 import { FeaturesService } from './features-service';
+import { TabListDescription } from '../tab-list-description/tab-list-description';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-description',
-  imports: [ModalFeatures],
+  imports: [ModalFeatures, TabListDescription],
   templateUrl: './product-description.html',
   styleUrl: './product-description.css',
 })
 export class ProductDescription {
   buyProduct = input.required<ProductType>();
+  index: WritableSignal<string> = signal<string>('1');
 
-  constructor(private featuresService: FeaturesService) {
-  }
+  constructor(private featuresService: FeaturesService,
+              private router: Router,) {}
 
   public openModalFeatures(index: string): void {
     this.featuresService.triggerFeatures(index);
+  }
+
+  public navigateTo(url: string): void {
+    this.router.navigate([url]).then(() => {});
   }
 }
