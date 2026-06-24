745300ecae93a02126adeaed47d453ff82b3103c
migel <migelangelo10@gmail.com>
2026-06-24 11:26:26 +0300

    sign in added

---
+++ src/app/components/signin-code-form/signin-code-form.ts
@@ -0,0 +1,153 @@
+import {
+  Component,
+  effect,
+  ElementRef,
+  EventEmitter,
+  input,
+  Output,
+  signal, viewChild,
+  viewChildren,
+  WritableSignal
+} from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signin-code-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './signin-code-form.html',
+  styleUrl: './signin-code-form.css',
+})
+export class SigninCodeForm {
+
+  @Output() spinnerEnd: EventEmitter<boolean> = new EventEmitter();
+
+  userEmail = input.required<string>();
+  spinnerActive = input.required<boolean>();
+
+  public digitActive: WritableSignal<number> = signal<number>(1);
+  readonly digits = viewChildren<ElementRef<HTMLElement>>('digit');
+  readonly submitBtn = viewChild<ElementRef<HTMLButtonElement>>('submit');
+
+  public codeError: WritableSignal<boolean> = signal<boolean>(false);
+  public codeShake: WritableSignal<boolean> = signal<boolean>(false);
+
+  public codeForm: FormGroup = new FormGroup({
+    digitInput: new FormControl('', [Validators.required, Validators.maxLength(6)]),
+  });
+
+  constructor() {
+
+    effect(() => {
+      const sActive = this.spinnerActive();
+
+      if (sActive) {
+        setTimeout(() => {
+          this.spinnerEnd.emit(false);
+        }, 1000);
+      }
+    });
+  }
+
+  public onSubmit(): void {
+
+    if (this.codeForm.valid) {
+
+      let value: string = this.codeForm.value.digitInput;
+
+      if (value !== "000000") {
+        this.codeShake.set(true);
+        this.codeError.set(true);
+
+        setTimeout(() => {
+          this.codeShake.set(false);
+          this.codeForm.reset();
+          this.reset();
+        }, 800);
+      }
+    }
+  }
+
+  private reset(): void {
+    const codes = this.digits();
+    if (codes.length > 0) {
+      for (let i = 0; i < codes.length; i++) {
+
+        const code = codes[i];
+        let target = code.nativeElement.firstElementChild?.firstElementChild;
+
+        if (target !== null && target !== undefined) {
+          target.innerHTML = '';
+          target.classList.remove('digit-enter');
+        }
+      }
+    }
+    this.digitActive.set(1);
+  }
+
+  public onInputChange(e: KeyboardEvent): void {
+
+    const value = e.key;
+
+    const codes = this.digits();
+    if (codes) {
+      for (let i = 0; i < codes.length; i++) {
+
+        const code = codes[i];
+        const id = code.nativeElement.dataset['id'];
+
+        if (id !== undefined) {
+          let index = parseInt(id);
+
+          if (index === this.digitActive()) {
+
+            let target = code.nativeElement.firstElementChild?.firstElementChild;
+
+            if (target !== undefined && target !== null) {
+
+              if (value !== 'Backspace' && !target.classList.contains('digit-enter')) {
+                target.innerHTML = value;
+                target.classList.add('digit-enter');
+
+                if (index < 6) {
+                  index++;
+                  this.digitActive.set(index);
+                }
+                else {
+                  const btn = this.submitBtn()?.nativeElement;
+                  if (btn) {
+                    btn.click();
+                  }
+                }
+
+              }
+              else if (value === 'Backspace' && i > 0 && index > 1) {
+
+                target.innerHTML = '';
+                target.classList.remove('digit-enter');
+                let backTarget = codes[i - 1].nativeElement;
+                let codeTarget = backTarget.firstElementChild?.firstElementChild;
+
+                if (codeTarget !== undefined && codeTarget !== null) {
+                  codeTarget.classList.remove('digit-enter');
+                  codeTarget.classList.add('digit-exit');
+
+                  setTimeout(() => {
+                    codeTarget.innerHTML = '';
+                    codeTarget.classList.remove('digit-exit');
+                    index--;
+                    this.digitActive.set(index);
+                  }, 200);
+                }
+
+              }
+
+            }
+
+
+            break;
+          }
+        }
+      }
+    }
+  }
+}
--- src/app/pages/sign-in/sign-in.ts
+++ src/app/pages/sign-in/sign-in.ts
@@ -2,10 +2,12 @@ import { Component, DestroyRef, OnInit, signal, WritableSignal } from '@angular/
 import { RouterLink } from '@angular/router';
 import { PopUp } from '../../components/pop-up/pop-up';
 import { PopUpService } from '../../components/pop-up/pop-up-service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SigninCodeForm } from '../../components/signin-code-form/signin-code-form';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [RouterLink, PopUp],
+  imports: [RouterLink, PopUp, ReactiveFormsModule, SigninCodeForm],
   templateUrl: './sign-in.html',
   styleUrl: './sign-in.css',
 })
@@ -13,9 +15,23 @@ export class SignIn implements OnInit {
   public authBannerUrl: string =
     '--global-auth-image: url("/assets/images/categories/new/NEW_44.jpg");';
 
-  public popUpType: WritableSignal<string> = signal<string>('privacy policy');
+  public popUpType: WritableSignal<string> = signal<string>('');
+  public userEmail: WritableSignal<string> = signal<string>('');
+  public hasError: WritableSignal<boolean> = signal<boolean>(false);
+  public processing: WritableSignal<boolean> = signal<boolean>(false);
+  public processingDone: WritableSignal<boolean> = signal<boolean>(false);
+  public formActive: WritableSignal<boolean> = signal<boolean>(true);
+  public spinnerActive: WritableSignal<boolean> = signal<boolean>(false);
 
-  constructor(private destroyRef: DestroyRef, private popupService: PopUpService) {
+  public authForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    consent: new FormControl(false),
+  });
+
+  constructor(
+    private destroyRef: DestroyRef,
+    private popupService: PopUpService,
+  ) {
     this.destroyRef.onDestroy(() => {
       document.body.classList.remove('auth-kit');
     });
@@ -25,7 +41,54 @@ export class SignIn implements OnInit {
     document.body.classList.add('auth-kit');
   }
 
-  public openPopup(): void {
-    this.popupService.popUpOpen(this.popUpType());
+  get email() {
+    return this.authForm.get('email');
+  }
+
+  public openPopup(type: string): void {
+    this.popUpType.set(type);
+
+    requestAnimationFrame(() => {
+      this.popupService.popUpOpen(type);
+    });
+  }
+
+  public onSubmit(): void {
+    if (this.authForm.valid) {
+      this.hasError.set(false);
+
+      this.processing.set(true);
+      this.userEmail.set(this.authForm.value.email);
+
+      setTimeout(() => {
+        this.processing.set(false);
+        this.authForm.reset();
+        this.formActive.set(false);
+
+        setTimeout(() => {
+          this.formActive.set(true);
+          this.processingDone.set(true);
+        }, 300);
+      }, 2000);
+    } else {
+      this.hasError.set(true);
+    }
+  }
+
+  public onChange(): void {
+
+    this.spinnerActive.set(true);
+
+  }
+
+  public onSpinner(state: boolean): void {
+    this.spinnerActive.set(state);
+    this.formActive.set(state);
+    this.userEmail.set('');
+    this.processingDone.set(state);
+
+    setTimeout(() => {
+      this.formActive.set(true);
+    }, 300);
   }
 }
