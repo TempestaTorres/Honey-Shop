d905139a843cd9c8fc6907458c6eeb7e27c0fe50
migel <migelangelo10@gmail.com>
2026-06-25 08:13:40 +0300

    account footer added

--- src/app/account/account-footer/account-footer.ts
+++ src/app/account/account-footer/account-footer.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { PopUpService } from '../../components/pop-up/pop-up-service';
+import { PopUp } from '../../components/pop-up/pop-up';
 
 @Component({
   selector: 'app-account-footer',
-  imports: [],
+  imports: [PopUp],
   templateUrl: './account-footer.html',
   styleUrl: './account-footer.css',
 })
-export class AccountFooter {}
+export class AccountFooter {
+  public popUpType: WritableSignal<string> = signal<string>('');
+
+  constructor(private popupService: PopUpService) {}
+
+  public openPopup(type: string): void {
+    this.popUpType.set(type);
+
+    requestAnimationFrame(() => {
+      this.popupService.popUpOpen(type);
+    });
+  }
+}
